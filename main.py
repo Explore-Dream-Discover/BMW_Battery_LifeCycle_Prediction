c0bcea122a993898f3baee3a07bb6b5770e276ff
Explore-Dream-Discover <elangopavankumar@gmail.com>
2023-11-13 21:04:28 -0800

    VehicleDataWithColumns

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-import plotly
+import plotly.express as px
 import dash
+import pandas as pd
+data = pd.read_csv("data\Vehicle_data.csv")
+print(data)
+# Create a line plot using Plotly
+# fig = px.line(data, x='Time [s]', y='Throttle [%]', title='Time vs Velocity')
+
+# # Show the plot
+# fig.show()
