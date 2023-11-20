b79539a73438ce673a44a8c682fabf39981d0bee
Explore-Dream-Discover <elangopavankumar@gmail.com>
2023-11-20 07:28:48 -0800

    First line

--- app.py
+++ app.py
@@ -263,16 +263,16 @@ pie_plots = dbc.Container([
 
 app.layout = html.Div([#title
                         html.H1('BATTERY  HEATING DATA IN REAL DRIVING CYCLES', className='dashboard-title'),
-                        html.Div(cards,style={'display': 'flex','backgroundColor': 'lightblue', 'padding': '10px'}),
-                        html.Div(html.H3('1.Vehicle Data Analysis')),
-                        html.Div(Analog_plots,style={'display': 'flex','backgroundColor': 'lightblue', 'padding': '10px'}),
+                        # html.Div(cards,style={'display': 'flex','backgroundColor': 'lightblue', 'padding': '10px'}),
+                        # html.Div(html.H3('1.Vehicle Data Analysis')),
+                        # html.Div(Analog_plots,style={'display': 'flex','backgroundColor': 'lightblue', 'padding': '10px'}),
 
-                        html.Div(html.H3('2.Histogram plots for Vehicle data')),
-                        html.Div(histogram_plots,style={'display': 'flex','backgroundColor': 'lightblue', 'padding': '10px'}),
+                        # html.Div(html.H3('2.Histogram plots for Vehicle data')),
+                        # html.Div(histogram_plots,style={'display': 'flex','backgroundColor': 'lightblue', 'padding': '10px'}),
 
 
-                        html.Div(html.H3('3.Pie plots for Vehicle data')),
-                        html.Div(pie_plots,style={'display': 'flex','backgroundColor': 'lightblue', 'padding': '10px'}),
+                        # html.Div(html.H3('3.Pie plots for Vehicle data')),
+                        # html.Div(pie_plots,style={'display': 'flex','backgroundColor': 'lightblue', 'padding': '10px'}),
 
                         # html.Div(Bullet_plots,style={'display': 'flex','backgroundColor': 'lightblue', 'padding': '10px'}),
                         # html.H3("3.Time vs. Temperature"),
