d5f5e33d686cd43eb131dd62a411ba76889eda38
Explore-Dream-Discover <elangopavankumar@gmail.com>
2023-12-05 00:27:33 -0800

    Density map

--- app.py
+++ app.py
@@ -529,15 +529,15 @@ app.layout = html.Div(
                 "padding": "35px",
             },
         ),
-        # html.Div(html.H3("4.Density Heat map Vs histogram for Torque Distribution")),
-        # html.Div(
-        #     twoD_histogram_plots,
-        #     style={
-        #         "display": "flex",
-        #         "backgroundColor": "lightblue",
-        #         "padding": "35px",
-        #     },
-        # ),
+        html.Div(html.H3("4.Density Heat map Vs histogram for Torque Distribution")),
+        html.Div(
+            twoD_histogram_plots,
+            style={
+                "display": "flex",
+                "backgroundColor": "lightblue",
+                "padding": "35px",
+            },
+        ),
         # html.Div(html.H3("5.Battery Data analysis")),
         #         html.Div(
         #     [curr_vs_Volt,
