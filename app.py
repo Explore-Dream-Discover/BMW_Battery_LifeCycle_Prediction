b4635cfb5916143d68393c65aaaa13ea5b3d7589
Explore-Dream-Discover <elangopavankumar@gmail.com>
2023-12-06 06:14:59 -0800

    Final_Result V1

--- app.py
+++ app.py
@@ -548,26 +548,26 @@ app.layout = html.Div(
 
 
 
-        html.Div(html.H3("5.Battery Data analysis")),
-                html.Div(
-            [curr_vs_Volt,
-            radar_speed_volt],
+        # html.Div(html.H3("5.Battery Data analysis")),
+        #         html.Div(
+        #     [curr_vs_Volt,
+        #     radar_speed_volt],
             
-            style={
-                "display": "flex",
-                "backgroundColor": "lightblue",
-                "padding": "35px",
-            },),
+        #     style={
+        #         "display": "flex",
+        #         "backgroundColor": "lightblue",
+        #         "padding": "35px",
+        #     },),
 
        
-        html.Div(
-            [radar_soc_tmp,sunburst_soc_tmp],
+        # html.Div(
+        #     [radar_soc_tmp,sunburst_soc_tmp],
             
-            style={
-                "display": "flex",
-                "backgroundColor": "lightblue",
-                "padding": "35px",
-            },)    
+        #     style={
+        #         "display": "flex",
+        #         "backgroundColor": "lightblue",
+        #         "padding": "35px",
+        #     },)    
     ])
 
 
